73dba7dd1d8463d74edf6fe60f4e83e786d4f007
lionsoft-pierre <pierre@lionsoft.ca>
2021-01-06 13:52:34 -0500

    Revert "Renamed files to match repo and npm name."

--- src/ng2-pdfjs-viewer.module.ts
+++ src/ng2-pdfjs-viewer.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PdfJsViewerComponent } from './pdf-js-viewer.component';
+import { PdfJsViewerComponent } from './ng2-pdfjs-viewer.component';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [PdfJsViewerComponent],
-  exports: []
+  exports: [PdfJsViewerModule]
 })
 export class PdfJsViewerModule {}
